e06cb464bc35829f91078bf8d22116a48af1b34d
Emil Bayanov <atex-69@mail.ru>
2023-06-07 04:18:31 +0300

    end write jsx

--- src/utils/Movies.js
+++ src/utils/Movies.js
@@ -71,30 +71,6 @@ const movies = [
         duration: '1ч 42м',
         image: 'https://i116.fastpic.org/big/2022/0313/66/6322ced78f7d6298830b2aee708bb966.jpg',
     },
-    {
-        id: 13,
-        title: 'Рудбой',
-        duration: '1ч 42м',
-        image: 'https://i116.fastpic.org/big/2022/0313/43/0df9629e7b7d9eb9311f57eab3c6c943.jpg',
-    },
-    {
-        id: 14,
-        title: 'Скейт — кухня',
-        duration: '1ч 42м',
-        image: 'https://i116.fastpic.org/big/2022/0313/97/152b96cb2cc66114f4be1ece0ae1a297.jpg',
-    },
-    {
-        id: 15,
-        title: 'Война искусств',
-        duration: '1ч 42м',
-        image: 'https://i116.fastpic.org/big/2022/0313/b2/845ba37a8685531337c6f4749c5beeb2.jpg',
-    },
-    {
-        id: 16,
-        title: 'Зона',
-        duration: '1ч 42м',
-        image: 'https://i116.fastpic.org/big/2022/0313/47/026b667962b7af7f99470cb0d821c547.jpg',
-    },
-];
+    ];
 
 export default movies;
