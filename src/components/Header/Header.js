ec5c81284e15c0a518f7c4a79332583082ab1824
Emil Bayanov <atex-69@mail.ru>
2023-06-11 06:11:28 +0300

    add func app

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,17 @@
 import './Header.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../../images/logo.svg';
-import NavTab from '../NavTab/NavTab';
+import NavAuth from '../NavTab/NavTab';
 import Navigation from '../Navigation/Navigation';
 
-const Header = ({ loggedIn }) => {
+const Header = ({ loggedIn, isLoading }) => {
+    const { pathname } = useLocation();
     return (
-        <header className={`header ${!loggedIn ? 'header_type_auth' : ''}`}>
+        <header className={`header ${pathname !== '/' ? '' : 'header_type_auth'}`}>
             <Link to="/" className="header__link">
                 <img className="header__logo" src={logo} alt="Логотип Movies Explorer"></img>
             </Link>
-            {!loggedIn && <NavTab />}
-            {loggedIn && <Navigation />}
+            {isLoading ? '' : loggedIn ? <Navigation /> : <NavAuth />}
         </header>
     );
 };
